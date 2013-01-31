67cf15d20a7f9be514d390898bffe6343bfb9b81
swcool <ac2k888@yahoo.com>
2013-01-31 12:48:57 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+markdownViewer
+==============
+
+markdownViewer is a extention for chrome, used for reading markdown file.
+
+Features:
+==============
+
+1. Reading markdown file
+2. Auto reload local file when file is changed
+3. Highlight the code area
+4. Show outline beside the content
+5. Buttons for GoTop, ViewSource and GoBottom
+
+License
+================
+
+markdownViewer is released under the MIT license:
+
+> The MIT License
+>
+>
+> Permission is hereby granted, free of charge, to any person obtaining a copy
+> of this software and associated documentation files (the "Software"), to deal
+> in the Software without restriction, including without limitation the rights
+> to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+> copies of the Software, and to permit persons to whom the Software is
+> furnished to do so, subject to the following conditions:
+>
+> The above copyright notice and this permission notice shall be included in
+> all copies or substantial portions of the Software.
+>
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+> IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+> FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+> AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+> LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+> OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+> THE SOFTWARE.
+
+Thanks
+================
+
+* markdownreader.js: [https://github.com/yaniswang/markdownReader](https://github.com/yaniswang/markdownReader)
+* showdown.js: [https://github.com/coreyti/showdown](https://github.com/coreyti/showdown)
+* prettify: [http://code.google.com/p/google-code-prettify/](http://code.google.com/p/google-code-prettify/)
+* GitHub: [https://github.com/](https://github.com/)
---
+++ markdownreader.js
@@ -0,0 +1,101 @@
+(function(document) {
+
+	if (document.doctype) return;
+
+	var link = document.createElement('link');
+	link.rel = 'stylesheet';
+	link.href = chrome.extension.getURL('markdownreader.css');
+	document.head.appendChild(link);
+
+	link = document.createElement('link');
+	link.rel = 'stylesheet';
+	link.href = chrome.extension.getURL('prettify.css');
+	document.head.appendChild(link);
+	document.body.innerHTML = '<div id="markdown-container"></div><div id="markdown-outline">'+ 
+                            '</div><div id="markdown-backTop" onclick="window.scrollTo(0,0);">'+
+                            '</div><div id="markdown-bottom" onclick="window.scrollTo(0, document.body.scrollHeight);"></div>'+
+                            '<div id="markdown-raw" onclick="window.location=\'view-source:\' + window.location.href;"></div>';
+
+	window.onresize = showOutline;
+
+	var markdownConverter = new Showdown.converter();
+	var lastText = null;
+
+	function updateMarkdown(text) {
+		if (text !== lastText) {
+			lastText = text;
+			markdownConverter.makeHtml(lastText)
+			document.getElementById('markdown-container').innerHTML = markdownConverter.makeHtml(lastText);
+			prettyPrint();
+			updateOutline();
+		}
+	}
+
+	function updateOutline() {
+		var arrAllHeader = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+		var arrOutline = ['<ul>'];
+		var header, headerText;
+		var id = 0;
+		var level = 0,
+			lastLevel = 1;
+		var levelCount = 0;
+		for (var i = 0, c = arrAllHeader.length; i < c; i++) {
+			header = arrAllHeader[i];
+			headerText = header.innerText;
+
+			header.setAttribute('id', id);
+
+			level = header.tagName.match(/^h(\d)$/i)[1];
+			levelCount = level - lastLevel;
+
+			if (levelCount > 0) {
+				for (var j = 0; j < levelCount; j++) {
+					arrOutline.push('<ul>');
+				}
+			} else if (levelCount < 0) {
+				levelCount *= -1;
+				for (var j = 0; j < levelCount; j++) {
+					arrOutline.push('</ul>');
+				}
+			};
+			arrOutline.push('<li>');
+			arrOutline.push('<a href="#' + id + '">' + headerText + '</a>');
+			arrOutline.push('</li>');
+			lastLevel = level;
+			id++;
+		}
+		arrOutline.push('</ul>')
+		var outline = document.getElementById('markdown-outline');
+		if(arrOutline.length > 2){
+			outline.innerHTML = arrOutline.join('');
+			showOutline();
+		}
+		else outline.style.display = 'none'; 
+	}
+
+	function showOutline() {
+		var outline = document.getElementById('markdown-outline');
+		var markdownContainer = document.getElementById('markdown-container');
+		outline.style.left = markdownContainer.offsetLeft + markdownContainer.offsetWidth + 10 + 'px';
+		outline.style.display = 'block';
+	}
+
+	var xmlhttp = new XMLHttpRequest();
+	var fileurl = location.href,
+		bLocalFile = /^file:\/\//i.test(fileurl);
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status != 404) {
+			updateMarkdown(xmlhttp.responseText);
+		}
+	};
+
+	function checkUpdate() {
+		xmlhttp.abort();
+		xmlhttp.open("GET", fileurl + '?rnd=' + new Date().getTime(), true);
+		xmlhttp.send(null);
+		if (bLocalFile) setTimeout(checkUpdate, 500);
+	}
+
+	checkUpdate();
+
+}(document));
